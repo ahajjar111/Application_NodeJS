d9dbdfbd1d7bf539901f6f415dc870ef6eec2781
Ayman Hajjar <aymanhajjar9@gmail.com>
2022-01-18 17:29:01 +0100

    Add Application.ts and modify in .json files

---
+++ CR.md
@@ -0,0 +1,19 @@
+## Compte rendu
+## Ayman Hajjar
+
+## TD1 : une application Node.js
+
+Q2. Que pouvez-vous dire sur le fichier package.json ? Sur le fichier package-lock.json ?
+
+Q3. Installez avec npm la bibliothèque systeminformation. Quel impact cette opération a-t-elle sur votre dépôt Git ? Dans package.json, quelle différence y a-t-il entre dependencies et devDependencies ?
+
+    Les deux fichiers "package.json" et "package-lock.json" sont modifiés.
+    (Voir les screenshots)
+
+Q4. 
+Voir screenshot TD1
+
+
+Q5. Testez le fonctionnement de votre application. Vous pouvez utiliser l’outil curl. À votre avis, pourquoi utilise-t-on ce formalisme pour construire l’URL de l’API ?
+
+screenshots fonctionnement
---
+++ src/Application.ts
@@ -0,0 +1,66 @@
+const http = require('http');
+
+const si = require('systeminformation');
+const { resourceLimits } = require('worker_threads');
+
+const hostname = 'localhost';
+const port = 8080;
+
+const result=[];
+
+Promise.all([
+
+    // CPU
+    si.cpu(function(data) {
+       result.push(data);
+    }),
+
+    // Memo
+    si.mem(function(data) {
+        result.push(data);
+    }),
+
+    //OS
+    si.osInfo(function(data) {
+    result.push(data);
+    }),
+
+    //currentLoad 
+    si.currentLoad(function(data) {
+        result.push(data);
+    }),
+
+    //diskLayout Information
+    si.diskLayout(function(data) {
+    result.push(data);
+    }),
+
+    //current
+    si.networkInterfaces(function(data) {
+        result.push(data);
+    })
+    ]).then((result) =>{
+
+        console.log(result);
+
+    });
+
+const requestListner = async function(req, res) {
+    // const sysinfo = await getSysInfo();
+    if(req.url != '/api/v1/sysinfo'){
+        res.writeHead(200,'{"Content-Type: application/json"}');
+        return res.end('404 Not Found');
+    }
+    else{
+        res.writeHead(200,'{"Content-Type: application/json"}');
+        return res.end(JSON.stringify(result))
+    }
+};
+
+const server = http.createServer(requestListner);
+
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/api/v1/sysinfo`);
+    });
+ 
