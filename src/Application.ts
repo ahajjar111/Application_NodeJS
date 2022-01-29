681bea844acd44c2e1af82d93450877013d426c1
Ayman Hajjar <aymanhajjar9@gmail.com>
2022-01-29 20:53:13 +0100

    process.env

--- src/Application.ts
+++ src/Application.ts
@@ -60,7 +60,7 @@ const requestListner = async function(req, res) {
 const server = http.createServer(requestListner);
 
 
-server.listen(port, hostname, () => {
+server.listen(port || process.env.PORT, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/api/v1/sysinfo`);
     });
  
